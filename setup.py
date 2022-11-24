1f9f36fc9fd60b542ce325db2b4011b7a1515328
Ankur Shrivastava <ankur@Alfa.com>
2022-11-24 15:56:44 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup,find_packages
+from typing import List
+
+# Declaring variables for setup functions
+PROJECT_NAME="housing-predictor",
+VERSION="0.0.1",
+AUTHOR="Ankur Shrivastava",
+DESCRIPTION="we are going to predict houses pricing"
+PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+
+def get_requirements_list()->List(str):
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+    
+
+setup(
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+install_requires=get_requirements_list()
+)
+
+if __name__=="__main__":
+    print(get_requirements_list())
