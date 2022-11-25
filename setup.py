5c373f9899ef7414738cc99f1a900a7a4ad95354
Ankur Shrivastava <ankur@Alfa.com>
2022-11-25 14:01:12 +0530

    Logger and Exeption Updated

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup,find_packages
 from typing import List
 
-# Declaring variables for setup functions
-PROJECT_NAME="housing-predictor",
-VERSION="0.0.4",
-AUTHOR="Ankur Shrivastava",
-DESCRIPTION="we are going to predict houses pricing"
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.7"
+AUTHOR="Ankur Shrivastava"
+DESRCIPTION="This is a house price predictor project"
+
 REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
 
 
 def get_requirements_list() -> List[str]:
@@ -17,14 +20,20 @@ def get_requirements_list() -> List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines().remove("-e .")
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
 
 setup(
 name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
-description=DESCRIPTION,
-packages=find_packages(),
+description=DESRCIPTION,
+packages=find_packages(), 
 install_requires=get_requirements_list()
 )
 
